f0869f783c90af479e79cb356e9b9e1ed749f845
macbook pro <macbookpro@MacBook-Pro-de-macbook.local>
2026-07-16 14:55:00 +0000

    add

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -7,11 +7,12 @@ const roleMiddleware = require('../middlewares/role.middleware');
 
 // Liste des produits
 router.get('/', productController.getAll);
-router.get('/count', authMiddleware, roleMiddleware('admin'), productController.getCount);
+
 router.get('/categories/last-products', productController.getLastProductsByCategories);
 router.get('/:id', productController.getById);
+router.get('/count', authMiddleware, roleMiddleware('admin'), productController.getCount);
 router.get('/:id/category', productController.getByCategory);
-router.get('/count/:categoryId', authMiddleware, roleMiddleware('admin'), productController.getCountByCategory)
+router.get('/count/:categoryId', authMiddleware, roleMiddleware('admin'), productController.getCountByCategory);
 
 // Création d’un produit avec image
 router.post('/', authMiddleware, roleMiddleware('admin'), upload.single('image'), productController.create);
