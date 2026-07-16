d3f7a42ee1ba3b4b1ae7c7d2ef95778001b86644
macbook pro <macbookpro@MacBook-Pro-de-macbook.local>
2026-07-16 15:20:47 +0000

    order des routes

--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -8,17 +8,16 @@ const roleMiddleware = require('../middlewares/role.middleware');
 
 const optionalAuth = require('../middlewares/optionalAuth.middleware');
 
-router.post('/', optionalAuth, orderController.store);
-
 // Récupérer toutes les commandes (admin uniquement)
 router.get('/', authMiddleware, roleMiddleware('admin'), orderController.getAll);
-//Récupérer une commande par ID (admin uniquement)
-router.get('/:id', authMiddleware, roleMiddleware('admin'), orderController.getOne);
 router.get('/count', authMiddleware, roleMiddleware('admin'), orderController.getOrderCount);
-router.get('/count/:status', authMiddleware, roleMiddleware('admin'), orderController.getOrderCountByStatus);
+//Récupérer une commande par ID (admin uniquement)
 router.get('/count/clients',authMiddleware, roleMiddleware('admin'), orderController.getClientCount);
-
+router.get('/:id', authMiddleware, roleMiddleware('admin'), orderController.getOne);
 // Mettre à jour le statut d’une commande (admin uniquement)
 router.put('/:id/', authMiddleware, roleMiddleware('admin'), orderController.updateStatus);
+router.get('/count/:status', authMiddleware, roleMiddleware('admin'), orderController.getOrderCountByStatus);
+
+router.post('/', optionalAuth, orderController.store);
 
 module.exports = router;
